3b1d0844ff2c67f7ad6e0251cd8f5c7289c6b72e
Benjamin Royston <benjamin.royston@gmail.com>
2025-06-23 21:52:20 -0500

    template added git branch -M main
    n

--- config/firebase.js
+++ config/firebase.js
@@ -1,24 +1,26 @@
-import { initializeApp } from "firebase/app";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import Constants from "expo-constants";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { initializeApp } from 'firebase/app';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import Constants from 'expo-constants';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// add firebase config
 const firebaseConfig = {
   apiKey: Constants.expoConfig?.extra?.apiKey,
   authDomain: Constants.expoConfig?.extra?.authDomain,
   projectId: Constants.expoConfig?.extra?.projectId,
   storageBucket: Constants.expoConfig?.extra?.storageBucket,
   messagingSenderId: Constants.expoConfig?.extra?.messagingSenderId,
-  appId: Constants.expoConfig?.extra?.appId,
+  appId: Constants.expoConfig?.extra?.appId
 };
 
-// initialize firebase
 const app = initializeApp(firebaseConfig);
 
-// initialize auth; only for native platforms (Android and iOS)
 const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
+  persistence: getReactNativePersistence(AsyncStorage)
 });
 
-export { auth };
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, db, storage };
